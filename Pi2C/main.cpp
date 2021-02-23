b486813f75ceef801851cbc6881fd8e10dd22092
a0939946923 <a0939946923@gmail.com>
2021-02-24 04:21:03 +0800

    no message

--- Pi2C/main.cpp
+++ Pi2C/main.cpp
@@ -11,7 +11,7 @@ int main(){
 	
 	char tmp[11];
 	while(1){
-		int ret = interface->i2cRead(tmp, 20);
+		int ret = interface->i2cRead(tmp, 16);
 		tmp[10] = '\0';
 		//if(ret != -1 && tmp[0] != 0){
 			
